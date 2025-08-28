f0bc66a2bc3320e9e48054d91a06a653478cc08c
Manas Kumar Maji <maji@adobe.com>
2025-08-28 16:36:14 +0530

    fix: add custom before tansform script

---
+++ tools/importer/import.custom.js
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2025 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+/**
+ * A map of custom parser names to imported parser functions.
+ *
+ * eg.
+ * {
+ *   myParser: customParser1,
+ * }
+ */
+export const customParsers = {};
+
+/**
+ * An array of custom page elements to parse.
+ * The name is the parser name.
+ * If the element is a string, it will be used as a selector to the element to parse.
+ * If the element is not provided, the parser will be applied to the main element.
+ *
+ * eg.
+ * [
+ *   { name: 'myParser', element: 'selector' },
+ * ]
+ */
+export const customElements = [];
+
+/**
+ * Custom transformers
+ */
+export const customTransformers = {
+    inject: (hookName, element, { document }) => {
+        if (hookName === 'beforeTransform') {
+            try {
+                // remove site header for NRMA Home Loans pages
+                document.querySelector('#aem-header-iframe-container')?.remove();
+                document.querySelector('.cmp-header-v2')?.remove();
+            
+                // Remove site footer for NRMA pages
+                document.querySelector('#aem-footer-iframe-container')?.remove();
+                document.querySelector('.cmp-footer--generic-template')?.remove();
+        
+                // remove chatbot for NRMA Home Loans pages
+                document.querySelector('#nuanMessagingFrame')?.remove();
+                document.querySelector('#chatXfModal')?.remove();
+            
+              } catch (e) {
+                // noop
+              }
+        
+        }
+      },
+};
